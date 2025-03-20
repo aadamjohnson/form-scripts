8c1c282c4e059a8d9246d248a9daa6c798629560
aadamjohnson <69766635+aadamjohnson@users.noreply.github.com>
2025-03-19 21:06:02 -0500

    Update display-rug-name.js

--- display-rug-name.js
+++ display-rug-name.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const rugNameInput = document.querySelector('input[name="117324478"]');
-    const htmlBlock = document.querySelector("#your-html-block-id");
-
-    if (rugNameInput && htmlBlock) {
-        rugNameInput.addEventListener("input", function() {
-            htmlBlock.innerText = rugNameInput.value;
-        });
-    }
-});
+console.log("JavaScript is working!");
